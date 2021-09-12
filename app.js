653dba9019493e21027582cdb9827a051269fde9
RalphM-02 <80824378+RalphM-02@users.noreply.github.com>
2021-09-12 12:52:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const express = require("express");
 const app = new express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+var hbs = require('hbs');
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+var server = app.listen(3000, function(){
+    console.log('Node server running');
+});
