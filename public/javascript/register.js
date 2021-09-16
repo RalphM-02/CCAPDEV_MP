f5afc1c65aa5583f0a0343ee10fe6f647467e5cf
RalphM-02 <80824378+RalphM-02@users.noreply.github.com>
2021-09-16 20:20:50 +0800

    registration

--- public/javascript/register.js
+++ public/javascript/register.js
@@ -14,4 +14,26 @@ $(document).ready(function(){
         });
     });
 
+    $('#password').keyup(function(){
+        if($('#password').length < 8){
+            $("#password-error").text('Password must be 8 or more characters long');
+            $("#register-submit").prop('disabled', true);
+        }
+        else{
+            $("#password=error").text("Password is valid");
+            $("#register-submit").prop('disabled', false);
+        }
+    });
+
+    $("#confirmpw").keyup(function(){
+        if($("#password").value() != $("#confirmpw").value()){
+            $("#confirmpw-error").text("Passwords do not match");
+            $("#register-submit").prop('disabled', true);
+        }
+        else{
+            $("#confirmpw-error").text("Passwords match");
+            $("#register-submit").prop('disabled', false);
+        }
+    });
+
 });
